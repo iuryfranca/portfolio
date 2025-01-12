03fe6829089d9c5be6edc894a9d034996bd80d85
Iury França <iurygfranca@gmail.com>
2025-01-11 23:38:55 -0400

    refactor: formatting code 📦

--- app/page.tsx
+++ app/page.tsx
@@ -7,24 +7,24 @@ import { Particles } from '@/components/ui/particles'
 import { ModeToggle } from '@/components/mode-toggle'
 
 export default function Home() {
-  const { theme } = useTheme()
-  const [color, setColor] = useState('#000000')
+	const { theme } = useTheme()
+	const [color, setColor] = useState('#000000')
 
-  useEffect(() => {
-    setColor(theme === 'dark' ? '#ffffff' : '#000000')
-  }, [theme])
+	useEffect(() => {
+		setColor(theme === 'dark' ? '#ffffff' : '#000000')
+	}, [theme])
 
-  return (
-    <div className='relative max-w-7xl mx-auto flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl'>
-      <ModeToggle />
+	return (
+		<div className="relative max-w-7xl mx-auto flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
+			<ModeToggle />
 
-      <Particles
-        className='absolute inset-0'
-        quantity={200}
-        ease={80}
-        color={color}
-        refresh
-      />
-    </div>
-  )
+			<Particles
+				className="absolute inset-0"
+				quantity={200}
+				ease={80}
+				color={color}
+				refresh
+			/>
+		</div>
+	)
 }
